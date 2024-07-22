684cfe9b680e94fdb840084cbc767560c3c49757
Joseph Morukhuladi <morukhuladi.j.kabelo@gmail.com>
2024-07-22 17:19:00 +0200

    Version 1.0.1

--- Malo.Web/Helpers/Bundles/AppBundles.cs
+++ Malo.Web/Helpers/Bundles/AppBundles.cs
@@ -4,7 +4,7 @@ namespace Malo.Web.Helpers.Bundles
 {
     public class AppBundles
     {
-        public static string MaloVersion = "1.0.0";
+        public static string MaloVersion = "1.0.1";
         public static long BundleVersion = DateTime.Now.Ticks;
         public static string[] GetCSSBundleFiles() =>  [ "/css/main.css", GetCSSFileForMaloVersion(MaloVersion) ];
         public static string[] GetJSBundleFiles() => [ GetJSFileForMaloVersion(MaloVersion), "/js/shared.ui.js", "/js/sidemenu.ui.js", "/js/app.js","/js/startup.js" ];
